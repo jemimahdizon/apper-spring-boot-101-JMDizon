62da0e9193fc548ca7805b6fa774e0831897893d
jemimahdizon <130464986+jemimahdizon@users.noreply.github.com>
2023-07-16 11:32:32 +0800

    Added base for capstone / codes from class

---
+++ day4/the-blog-service/src/main/java/com/apper/theblogservice/model/Blogger.java
@@ -0,0 +1,41 @@
+package com.apper.theblogservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "BLOGGER")
+@Data
+public class Blogger {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "COMPLETE_NAME")
+    private String name;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void setCreatedAt() {
+        createdAt = LocalDateTime.now();
+    }
+
+}
